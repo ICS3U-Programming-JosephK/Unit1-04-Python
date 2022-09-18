f0a0b6be3ee56ec43b642816306ac164f5831c9d
JosephK2006 <jiyong.kwon@stu.ocsb.ca>
2022-09-18 04:12:34 +0000

    added basic_math.py and area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Created by Joseph Kwon
+#Created on Sep 16, 2022
+#This program calculates the area and perimeter of a rectangle
+#With dimensions 4cm 20cm
+
+
+
+def main():
+  #this function calculates the area and perimeter of a rectangle 
+  print("If a rectangle has the dimensions:")
+  print("4cm x 20cm")
+  print("")
+  print("Area is {}cm^2".format(4*20))
+  print("Perimeter is {}cm".format(2*(4+20)))
+
+if __name__ == "__main__":
+  main()
